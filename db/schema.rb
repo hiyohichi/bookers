42c1814a1602d8725d09505bf6382a4430030a57
EC2 Default User <ec2-user@ip-172-31-22-80.ap-northeast-1.compute.internal>
2023-11-12 10:31:25 +0000

    Finish

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2023_11_11_073553) do
 
-  create_table "bookers", force: :cascade do |t|
-    t.string "title"
-    t.string "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.string "body"
